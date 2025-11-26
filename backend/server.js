21c2ce6bfdf4c5fc08a67fd93fd7313446f5c29c
kareem-ahmed <karim.1024112@stemoctober.moe.edu.eg>
2025-11-26 19:47:11 +0200

    finnishing the setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,31 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./.config/db.js");
+const Newspaper = require("./models/newspaper.model.js");
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post("/newspapers", (req, res) => {
+  const newspaper = req.body;
+
+  if (!newspaper.name || !newspaper.price || !newspaper.date) {
+    return res.status(400).json({ success: false, message: "please provide all fields"});
+  }
+
+  const newNewspaper = new Newspaper(newspaper);
+
+  try {
+    await newNewspaper.save();
+    res.status(201).json({ success: true, data: newNewspaper });
+  } catch (error) {
+    console.error("Error in create newspaper:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.listen(5000, () => {
-  console.log('Server started at http://localhost:5000');
+  connectDB();
+  console.log("Server started at http://localhost:5001");
 });
-
