420a7b8a8292383722a01b45cb0ded9c2f3a292a
kareem-ahmed <karim.1024112@stemoctober.moe.edu.eg>
2025-11-30 23:43:56 +0200

    please don't miss the repo up

--- frontend/src/pages/CreatePage/CreatePage.jsx
+++ frontend/src/pages/CreatePage/CreatePage.jsx
@@ -20,13 +20,37 @@ const CreatePage = () => {
       <div className="creation_box">
         <h2>Create product</h2>
         
-        <input placeholder= "Title" type="text" name="name" value={newNewspaper.name} onChange={(e) => setNewNewspaper  ({...newNewspaper, name: e.target.value })}/>
+        <input 
+          placeholder= "Title" 
+          type="text" 
+          name="name" 
+          value={newNewspaper.name} 
+          onChange={(e) => setNewNewspaper  ({...newNewspaper, name: e.target.value })}
+        />
         
-        <input placeholder= "Price" type="number" name="price" value={newNewspaper.price} onChange={(e) => setNewNewspaper  ({...newNewspaper, price: e.target.value })}/>
+        <input
+          placeholder="Price" 
+          type="number" 
+          name="price" 
+          value={newNewspaper.price} 
+          onChange={(e) => setNewNewspaper  ({...newNewspaper, price: e.target.value })}
+        />
         
-        <input placeholder= "Image" type="text" name="image" value={newNewspaper.image} onChange={(e) => setNewNewspaper  ({...newNewspaper, image: e.target.value })}/>
+        <input 
+          placeholder="Image" 
+          type="text" 
+          name="image" 
+          value={newNewspaper.image} 
+          onChange={(e) => setNewNewspaper  ({...newNewspaper, image: e.target.value })}
+        />
         
-        <input placeholder= "Date" type="text" name="date" value={newNewspaper.date} onChange={(e) => setNewNewspaper  ({...newNewspaper, date: e.target.value })}/>
+        <input 
+          placeholder= "Date" 
+          type="text" 
+          name="date" 
+          value={newNewspaper.date} 
+          onChange={(e) => setNewNewspaper  ({...newNewspaper, date: e.target.value })}
+        />
         
         <button onClick={handleCreate}>
           jlfk
