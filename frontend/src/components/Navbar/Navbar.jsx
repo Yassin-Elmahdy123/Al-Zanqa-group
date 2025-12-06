fbc80cf17ccd83aaf2a35762ac3dbb38408a4e3e
kareem-ahmed <karim.1024112@stemoctober.moe.edu.eg>
2025-12-06 02:33:47 +0200

    fully reseting the repositery to contenue working

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import "./Navbar.scss"
+import  { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <>
-      
-    </>
+      <nav className="navbar">
+        <Link to= "/">
+          <h2>Book Store</h2>
+        </Link>
+        <Link to="/cart">
+          <div className="navbag">
+            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-bookmark-heart" viewBox="0 0 16 16">
+              <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
+            </svg>
+            <span className="bag-quantity"><span>3</span></span>
+          </div>
+        </Link>
+      </nav>
   )
 }
 
