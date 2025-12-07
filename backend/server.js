57c36d8ca35e881cc336dbb09a48326c7b3509e3
Yassin-Elmahdy123 <yassinelmahdy201@gmail.com>
2025-12-07 03:35:59 +0200

    Solving some conflicts

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use("/api/newspapers", newspaperRoutes);
-app.use("/api/user", userRoutes);
-
-
+app.use("/api/users", userRoutes);
 app.listen(PORT, () => {
   connectDB();
   console.log("Server running on port", PORT);
